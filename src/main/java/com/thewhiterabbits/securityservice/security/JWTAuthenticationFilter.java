672d208686c0ce68e75bb6e76d641f210e4e1039
JULLIOT Benoît <benoitjulliot@gmail.com>
2021-04-11 01:07:55 +0200

    config spring security successful authentication

--- src/main/java/com/thewhiterabbits/securityservice/security/JWTAuthenticationFilter.java
+++ src/main/java/com/thewhiterabbits/securityservice/security/JWTAuthenticationFilter.java
@@ -1,15 +1,24 @@
 package com.thewhiterabbits.securityservice.security;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
@@ -25,5 +34,69 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
         logger.error("enter attemptAuthentication");
         return super.attemptAuthentication(request, response);
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+        logger.error("enter in JWTAuthenticationFilter method successfulAuthentication");
+        //recup user authentifié
+        User userSpring=(User) authResult.getPrincipal();
+        //recup roles
+        List<String> roles=new ArrayList<>();
+        userSpring.getAuthorities().forEach(role->{
+            roles.add(role.getAuthority());
+        });
+
+        //creation du token JWT
+        String jwt= JWT.create()
+                .withIssuer(request.getRequestURI())                                        //nom de l'appi qui à générée le token
+                .withSubject(userSpring.getUsername())                                      //user login
+                .withArrayClaim("roles", roles.toArray(new String[roles.size()]))     //tableau des roles du user
+                .withExpiresAt(new Date(System.currentTimeMillis()+SecurityParams.EXPIRATION))             //expiration du token 10 jours
+                .sign(Algorithm.HMAC256(SecurityParams.SECRET));
+
+        //ajout tokken au header de la reponse
+        response.addHeader(SecurityParams.JWT_HEADER_NAME, jwt);
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+
+        //ajout tokken au body de la reponse
+        response.getWriter().write(
+                "{\"" + SecurityParams.JWT_HEADER_NAME + "\":\"" + SecurityParams.HEADER_PREFIX+jwt + "\""
+        );
+        //ajouter user infos dans la reponse
+
+        response.getWriter().write(
+                ",\"username\":\"" + userSpring.getUsername() + "\""
+        );
+
+        int cpt = 0;
+        for (String role : roles) {
+            cpt++;
+            if(cpt == 1){
+                response.getWriter().write(
+                        ",\"Roles\":[");
+            }
+
+            response.getWriter().write(
+                    "\"" + role + "\""
+            );
+
+            if(cpt < roles.size()){
+                response.getWriter().write(
+                        ","
+                );
+            }else{
+                response.getWriter().write(
+                        "]"
+                );
+            }
+        }
+
+        response.getWriter().write(
+                "}"
+        );
+
+        //ResponseEntity.ok(jwt);
     }
 }
