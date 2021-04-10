075dc24570ea3020f920ca20840d81b3c79fcdd7
JULLIOT Benoît <benoitjulliot@gmail.com>
2021-04-10 18:27:15 +0200

    config mysql db for heroku

--- src/main/java/com/thewhiterabbits/securityservice/security/SecurityConfig.java
+++ src/main/java/com/thewhiterabbits/securityservice/security/SecurityConfig.java
@@ -1,14 +1,18 @@
 package com.thewhiterabbits.securityservice.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
